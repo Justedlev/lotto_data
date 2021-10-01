d0327781ffb44dea12ff5ccc53833988dcb1e714
Edward {•...•} <59051661+Justedlev@users.noreply.github.com>
2021-10-01 18:14:04 +0300

    Change: RestController to Controller

--- src/main/java/justedlev/lotto_data/controllers/LottoController.java
+++ src/main/java/justedlev/lotto_data/controllers/LottoController.java
@@ -9,13 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
-@RestController
+@Controller
 @CrossOrigin
 public class LottoController {
 
